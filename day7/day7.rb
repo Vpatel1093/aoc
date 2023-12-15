89a37cd25fe51988c4baf9ed0933e11a711431ff
Vishal Patel <v.patel1093@gmail.com>
2023-12-14 19:08:41 -0500

    day 7 part 2

--- day7/day7.rb
+++ day7/day7.rb
@@ -1,11 +1,12 @@
 TYPE_STRENGTHS = %i[five four full_house three two_pair one_pair high_card]
 CARD_STRENGTHS = %w[A K Q J T 9 8 7 6 5 4 3 2]
+CARD_STRENGTHS_WITH_JOKERS_WILD = %w[A K Q T 9 8 7 6 5 4 3 2 J]
 
-def day_6_part_1(file_name)
+def day_7(file_name, jokers_wild = false)
     hands_and_bids = get_hands_and_bids(file_name)
 
-    get_hand_types(hands_and_bids)
-    get_hand_rankings(hands_and_bids)
+    get_hand_types(hands_and_bids, jokers_wild)
+    get_hand_rankings(hands_and_bids, jokers_wild)
 
     puts hands_and_bids.map {|hand| hand[1] * hand[3] }.sum
 end
@@ -23,9 +24,26 @@ def get_hands_and_bids(file_name)
     return hands_and_bids
 end
 
-def get_hand_types(hands_and_bids)
+def get_hand_types(hands_and_bids, jokers_wild = false)
     hands_and_bids.each_with_index do |hand, index|
-        hand[2] = case hand[0].split('').tally.values.sort {|a, b| b <=> a}
+        j_count = 0
+    
+        hand_card_tally = hand[0].split('').tally
+        if hand_card_tally['J'] && jokers_wild
+            j_count = hand_card_tally['J']
+            hand_card_tally.delete('J')
+        end
+        hand_card_counts_sorted = hand_card_tally.values.sort {|a, b| b <=> a}
+
+        if jokers_wild
+            if j_count == 5
+                hand_card_counts_sorted << 5
+            else    
+                hand_card_counts_sorted[0] += j_count 
+            end
+        end
+        
+        hand[2] = case hand_card_counts_sorted
         when [5]
             :five
         when [4, 1]
@@ -44,9 +62,9 @@ def get_hand_types(hands_and_bids)
     end
 end
 
-def get_hand_rankings(hands_and_bids)
+def get_hand_rankings(hands_and_bids, jokers_wild = false)
     hands_and_bids.sort_by do |hand|
-        [TYPE_STRENGTHS.index(hand[2]), hand[0].split('').map { |card| CARD_STRENGTHS.index(card) }]
+        [TYPE_STRENGTHS.index(hand[2]), hand[0].split('').map { |card| jokers_wild ? CARD_STRENGTHS_WITH_JOKERS_WILD.index(card) : CARD_STRENGTHS.index(card) }]
     end.reverse.map.with_index do |hand, index|
         hand[3] = index + 1
     end
@@ -54,5 +72,7 @@ def get_hand_rankings(hands_and_bids)
     puts hands_and_bids
 end
 
-day_6_part_1("example.txt")
-day_6_part_1("input.txt")
+day_7("example.txt", false)
+day_7("input.txt", false)
+day_7("example.txt", true)
+day_7("input.txt", true)
