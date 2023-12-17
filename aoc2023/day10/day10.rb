2af7bf67f76bd7587814b0ba269f35751a390572
Vishal Patel <v.patel1093@gmail.com>
2023-12-17 14:00:52 -0500

    improve naming

--- aoc2023/day10/day10.rb
+++ aoc2023/day10/day10.rb
@@ -4,7 +4,7 @@ CARDINAL_DIRECTIONS = {
     "south" => [ 0, 1, "south" ],
     "west" => [ -1, 0, "west" ]
 }
-PIPE_CONNECTION_MAPPING = {
+PIPE_DIRECTION_MAPPING = {
     '|' => { "north" => CARDINAL_DIRECTIONS["north"], "south" => CARDINAL_DIRECTIONS["south"] },
     '-' => { "east" => CARDINAL_DIRECTIONS["east"], "west" => CARDINAL_DIRECTIONS["west"] },
     'L' => { "south" => CARDINAL_DIRECTIONS["east"], "west" => CARDINAL_DIRECTIONS["north"] },
@@ -19,22 +19,22 @@ def day_10(file_name)
     grid, start_location = get_grid_and_start_location(file_name)
     loop_length = 0
     
-    CARDINAL_DIRECTIONS.values.each do |direction|
+    CARDINAL_DIRECTIONS.values.each do |next_direction|
         current_x = start_location[0]
         current_y = start_location[1]
         current_pipe = ''
         loop_length = 0
-        current_direction = direction[2]
+        current_direction = next_direction[2]
         
         until current_pipe == 'S'
             loop_length += 1
-            current_x = current_x + direction[0]
-            current_y = current_y + direction[1]
+            current_x = current_x + next_direction[0]
+            current_y = current_y + next_direction[1]
             current_pipe = grid[current_x][current_y]
             
-            direction = PIPE_CONNECTION_MAPPING[current_pipe][current_direction]
-            break if direction.nil?
-            current_direction = direction[2]
+            next_direction = PIPE_DIRECTION_MAPPING[current_pipe][current_direction]
+            break if next_direction.nil?
+            current_direction = next_direction[2]
         end
 
         break if current_pipe == 'S'
