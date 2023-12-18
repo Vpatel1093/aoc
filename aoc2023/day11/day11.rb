df75161b462e86c3d1eb20ae28fba1337f704d4e
Vishal Patel <v.patel1093@gmail.com>
2023-12-18 18:36:56 -0500

    day 11 part 2

--- aoc2023/day11/day11.rb
+++ aoc2023/day11/day11.rb
@@ -1,4 +1,4 @@
-def day_11(file_name)
+def day_11(file_name, galaxy_age_factor)
     grid = get_grid(file_name)
     rows_to_expand, columns_to_expand = get_expansion_indices(grid)
     galaxy_coords = get_galaxy_coords(grid)
@@ -6,8 +6,8 @@ def day_11(file_name)
     sum_of_shortest_paths = 0
 
     galaxy_path_combinations.each do |galaxy_a, galaxy_b|
-        sum_of_shortest_paths += (galaxy_b[0] - galaxy_a[0]).abs + (([galaxy_a[0], galaxy_b[0]].min..[galaxy_a[0], galaxy_b[0]].max).to_a & rows_to_expand).count
-        sum_of_shortest_paths += (galaxy_b[1] - galaxy_a[1]).abs + (([galaxy_a[1], galaxy_b[1]].min..[galaxy_a[1], galaxy_b[1]].max).to_a & columns_to_expand).count
+        sum_of_shortest_paths += (galaxy_b[0] - galaxy_a[0]).abs + (([galaxy_a[0], galaxy_b[0]].min..[galaxy_a[0], galaxy_b[0]].max).to_a & rows_to_expand).count * (galaxy_age_factor - 1)
+        sum_of_shortest_paths += (galaxy_b[1] - galaxy_a[1]).abs + (([galaxy_a[1], galaxy_b[1]].min..[galaxy_a[1], galaxy_b[1]].max).to_a & columns_to_expand).count * (galaxy_age_factor - 1)
     end
 
     puts sum_of_shortest_paths
@@ -57,5 +57,8 @@ def get_galaxy_coords(grid)
     return coords
 end
 
-day_11("example.txt")
-day_11("input.txt")
+day_11("example.txt", 2)
+day_11("input.txt", 2)
+day_11("example.txt", 10)
+day_11("example.txt", 100)
+day_11("input.txt", 1000000)
